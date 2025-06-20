643eeaa239a76086773157176774002d7b5c1626
coopercal <coopercal@gmail.com>
2025-06-20 04:56:50 -0400

    Step 2

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,10 @@
 """
 Validates strings as palindromes.
 """
-def is_Palindrome(value:str):
-    if value != str:
+def is_Palindrome(value):
+    if not isinstance(value, str):
         raise ValueError
+    if value == "":
+        print("False")
+
+is_Palindrome("")
--- test_palindrome.py
+++ test_palindrome.py
@@ -9,3 +9,6 @@ from palindrome import is_Palindrome
 def test_ValueError():
     with pytest.raises(ValueError):
         is_Palindrome(10)
+
+def test_returnsFalseWithEmptyString():
+    assert is_Palindrome("") == "False"
