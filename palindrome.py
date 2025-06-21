f1b775dab2a8f23418b6aa6546157c287a4c8c0d
coopercal <coopercal@gmail.com>
2025-06-21 02:00:35 -0400

    Step Nine
    A little bit of clean up

--- palindrome.py
+++ palindrome.py
@@ -11,17 +11,14 @@ def is_Palindrome(value):
         return False
     else:
         value = value.lower()
+        value = value.replace(" ","")
         d = deque()
         for x in value:
             d.append(x)
-        print(d)
         e = deque(reversed(d))
-        print(e)
         if d == e:
-            print("true")
             return True
         if d != e:
-            print("false")
             return False
 
 
